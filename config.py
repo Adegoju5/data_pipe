c6fb4ce74f02d3c43044f6e62e59ecf7c2254205
Adegoju5 <115003200+Adegoju5@users.noreply.github.com>
2022-12-28 21:53:38 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,21 @@
+import os
+
+DB_DETAILS = {
+    'dev': {
+        'SOURCE DB': {
+            'DB_TYPE': 'mysql',
+            'DB_HOST': '139.99.209.131',
+            'DB_NAME': 'retail_db',
+            'DB_USER': 'retail_user',
+            'DB_PASS': 'itversity'
+        },
+
+        'TARGET DB': {
+            'DB_TYPE': 'mysql',
+            'DB_HOST': '139.99.209.131',
+            'DB_NAME': 'retail_db',
+            'DB_USER': 'retail_user',
+            'DB_PASS': 'itversity'
+        }
+    }
+}
